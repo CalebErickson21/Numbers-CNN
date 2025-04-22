adae568fcbe08193910d411cad0762dfa4471756
Caleb Erickson <coding.c9@gmail.com>
2025-04-22 03:15:43 +0100

    Initial send and request successful

--- backend/app.py
+++ backend/app.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 CORS(app)
 
 
-@app.route('/api/hello', methods=['GET'])
+@app.route('/api/hello', methods=['POST'])
 def hello():
-    return jsonify({"message": "Hello, World!"})
+    return jsonify({'message': 'Hello, World!', 'success': True})
 
 
 if __name__ == '__main__':
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+// Import dependencies
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+// Import styles
 import './App.css';
 
+// App component
 function App() {
+
+  const [text, setText] = useState('Blank');
+
+  const sendToBackend = async (e) => {
+    try {
+      e.preventDefault();
+      const response = await axios.post('http://localhost:5000/api/hello');
+      setText(response.data.message);
+    } catch (error) {
+      setText('Error');
+      console.error('Error calling backend:', error);
+    }
+  };
+
+
+  // Visible component
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={(e) => sendToBackend(e)}>Send to Backend</button>
+      <h1>{text}</h1>
     </div>
   );
 }
