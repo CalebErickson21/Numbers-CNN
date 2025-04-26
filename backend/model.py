b6073273581c092764de4062c7531cd7e0b92748
Caleb Erickson <coding.c9@gmail.com>
2025-04-26 01:59:36 +0100

    Added training data and work to further comment model.py for clarity

--- backend/model.py
+++ backend/model.py
@@ -1,9 +1,30 @@
 # Import dependencies
 import random # Used for shuffling data during training
 import time # Measure time taken per each epoch
+import struct # Used for reading binary files (MNIST dataset)
 import numpy as np # Used for matrix operations
 
-# Define neural network class
+### --- MNINST DATASET LOADER --- ###
+def load_mnist_images(filename):
+    with open(filename, 'rb') as f:
+        magic, num, rows, cols = struct.unpack(">IIII", f.read(16))
+        images = np.frombuffer(f.read(), dtype=np.uint8).reshape(num, rows * cols)
+        return [x.reshape(784, 1) / 255.0 for x in images]  # Normalize pixels to [0,1]
+
+
+def load_mnist_labels(filename):
+    with open(filename, 'rb') as f:
+        magic, num = struct.unpack(">II", f.read(8))
+        labels = np.frombuffer(f.read(), dtype=np.uint8)
+        return labels
+
+
+def vectorized_label(j):
+    e = np.zeros((10, 1))
+    e[j] = 1.0
+    return e
+
+### --- NUERAL NETWORK CLASS --- ###
 class Network(object):
 
     # Constructor to initialize the neural network with given sizes - automatically called when creating a network instance
@@ -145,3 +166,19 @@ def sigmoid(z):
 # Derivative of the sigmoid function, used for backpropagation
 def sigmoid_prime(z):
     return sigmoid(z)*(1-sigmoid(z))
+
+
+### --- MAIN EXECUTION --- ###
+if __name__ == "__main__":
+    # Load data from ./data/
+    train_imgs = load_mnist_images("data/train-images.idx3-ubyte")
+    train_labels = load_mnist_labels("data/train-labels.idx1-ubyte")
+    test_imgs = load_mnist_images("data/test-images.idx3-ubyte")
+    test_labels = load_mnist_labels("data/test-labels.idx1-ubyte")
+
+    training_data = list(zip(train_imgs, [vectorized_label(y) for y in train_labels]))
+    test_data = list(zip(test_imgs, test_labels))  # raw labels for evaluate()
+
+    # Initialize and train the network
+    net = Network([784, 30, 10])
+    net.SGD(training_data, epochs=10, mini_batch_size=10, eta=3.0, test_data=test_data)
